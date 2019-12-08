dc92c4dd60323279d4d6f640ac615b87188776d5
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-12-08 16:52:00 -0500

    tmp update

--- Triangle/main.cpp
+++ Triangle/main.cpp
@@ -1,6 +1,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <string.h>
 #include <cstdlib>
 #include <functional>
 #include <iostream>
@@ -8,6 +9,44 @@
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    for (const char *layerName : validationLayers) {
+        bool layerFound = false;
+        for (const auto &layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+        if (!layerFound) {
+            return false;
+        }
+    }
+    return true;
+}
+std::vector<const char *> getRequiredExtensions() {
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<const char *> extensions(glfwExtensions,
+                                         glfwExtensions + glfwExtensionCount);
+    // optional: debug messenger extensions
+    if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+    return extensions;
+}
 class HelloTriangleApplication {
 public:
     void run() {
@@ -31,6 +70,10 @@ private:
             nullptr);  // params: width, height, title, monitor, opengl related}
     }
     void createInstance() {
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error(
+                "validation layers requested, but not available");
+        }
         VkApplicationInfo appInfo = {};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;  // specify the type
                                                              // of the structure
@@ -42,6 +85,41 @@ private:
         VkInstanceCreateInfo createInfo = {};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+        auto extensions = getRequiredExtensions();
+        createInfo.enabledExtensionCount =
+            static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount =
+                static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+        VkResult result = vkCreateInstance(
+            &createInfo, nullptr,
+            &instance);  // params: structure of createInfo, custom allocator
+                         // callbacks(nullptr), variable stored the handle
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed tp create instance");
+        } else {
+            std::cout << "vkinstance created successfully" << std::endl;
+        }
+        uint32_t extensionCount = 0;
+        // judt request the number of extensions
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+                                               nullptr);
+        // create a vector to hold the extension details
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        // query the extension details
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+                                               extensions.data());
+        std::cout << "available extensions : " << std::endl;
+        for (const auto &extension : extensions) {
+            std::cout << "\t" << extension.extensionName << std::endl;
+        }
     }
     void initVulkan() { createInstance(); }
 
@@ -54,6 +132,8 @@ private:
 
     // when window is closed, clean up resources
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
         glfwTerminate();
     }
