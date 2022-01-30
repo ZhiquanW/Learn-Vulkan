9fd2036be021a5386199a79f0f519bc7f8ac5702
Zhiquan <zhiquan.wzq@gmail.com>
2022-01-30 14:31:03 -0500

    [completed] vk tutorial 04

--- triangle/src/includes/zw_pipeline.hpp
+++ triangle/src/includes/zw_pipeline.hpp
@@ -3,18 +3,34 @@
 #include <string>
 #include <vector>
 namespace zw {
-    struct PipelineConfigInfo {};
+    struct PipelineConfigInfo {
+        // A viewport basically describes the region of the framebuffer 
+        // that the output will be rendered to.
+        VkViewport viewport; 
+        VkRect2D scissor;
+        VkPipelineViewportStateCreateInfo viewportInfo;
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo multisampleInfo;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment; // a parameter in colorBlend
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+        VkPipelineLayout pipelineLayout = nullptr;
+        VkRenderPass renderPass = nullptr;
+        uint32_t subpass = 0;
+
+    };
     class ZWPipeline {
     public:
         ZWPipeline(ZWDevice& device, const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& configInfo);
-        ~ZWPipeline() {}
+        ~ZWPipeline();
         ZWPipeline(const ZWPipeline&) = delete;
         void operator=(const ZWPipeline&) = delete;
         static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
     private:
         static std::vector<char> readFile(const std::string& filepath);
         void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& configInfo);
-        void createShaderModule(const std::vector<char>& code, VkShaderModule& shaderModule);
+        void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
         ZWDevice& zwDevice;
         VkPipeline graphicsPipeline;
         VkShaderModule vertShaderModule;
--- triangle/src/zw_pipeline.cpp
+++ triangle/src/zw_pipeline.cpp
@@ -1,54 +1,199 @@
 #include "zw_pipeline.hpp"
 
+#include <cassert>
 #include <fstream>
 #include <iostream>
 namespace zw {
-    ZWPipeline::ZWPipeline(ZWDevice& device,
-        const std::string& vertFilepath,
-        const std::string& fragFilepath,
-        const PipelineConfigInfo& configInfo)
-        : zwDevice{ device } {
-        this->createGraphicsPipeline(vertFilepath, fragFilepath,configInfo);
-    }
-    std::vector<char> ZWPipeline::readFile(
-        const std::string& filename) {
-        std::ifstream file{ filename,
-                           std::ios::ate | std::ios::binary };
-        if (!file.is_open()) {
-            throw std::runtime_error("failed to open file: " +
-                filename);
-        }
-        size_t fileSize = (size_t)file.tellg();
-        std::vector<char> buffer(fileSize);
-        file.seekg(0);
-        file.read(buffer.data(), fileSize);
-        file.close();
-        return buffer;
-    }
+ZWPipeline::ZWPipeline(ZWDevice& device,
+                       const std::string& vertFilepath,
+                       const std::string& fragFilepath,
+                       const PipelineConfigInfo& configInfo)
+    : zwDevice{device} {
+    this->createGraphicsPipeline(vertFilepath, fragFilepath,
+                                 configInfo);
+}
 
-    void ZWPipeline::createGraphicsPipeline(
-        const std::string& vertFilepath,
-        const std::string& fragFilepath,
-        const PipelineConfigInfo& configInfo) {
-        auto vertCode = this->readFile(vertFilepath);
-        auto fragCode = this->readFile(fragFilepath);
-        std::cout << "Vertex Shader Code Size: " << vertCode.size() << "\n";
-        std::cout << "Fragment Shader Code Size: " << fragCode.size() << "\n";
+ZWPipeline::~ZWPipeline() {
+    vkDestroyShaderModule(this->zwDevice.device(),
+                          this->vertShaderModule, nullptr);
+    vkDestroyShaderModule(this->zwDevice.device(),
+                          this->fragShaderModule, nullptr);
+    vkDestroyPipeline(this->zwDevice.device(), this->graphicsPipeline,
+                      nullptr);
+}
+std::vector<char> ZWPipeline::readFile(const std::string& filename) {
+    std::ifstream file{filename, std::ios::ate | std::ios::binary};
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file: " + filename);
     }
-    void ZWPipeline::createShaderModule(
-        const std::vector<char>& code,
-        VkShaderModule& shaderModule) {
-        VkShaderModuleCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-        createInfo.codeSize = code.size();
-        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
-        if (vkCreateShaderModule(zwDevice.device(), &createInfo, nullptr, nullptr) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create shader module");
-        }
-    }
+    size_t fileSize = (size_t)file.tellg();
+    std::vector<char> buffer(fileSize);
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+    return buffer;
+}
 
-    PipelineConfigInfo ZWPipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height) {
-        PipelineConfigInfo configInfo{};
-        return configInfo;
+void ZWPipeline::createGraphicsPipeline(
+    const std::string& vertFilepath, const std::string& fragFilepath,
+    const PipelineConfigInfo& configInfo) {
+    assert(configInfo.pipelineLayout != nullptr &&
+           "Cannot create graphics pipeline: no pipelineLayout "
+           "provided in config info");
+    assert(configInfo.renderPass != nullptr &&
+           "Cannot create graphics pipeline: no renderPass provided in "
+           "config info");
+    auto vertCode = this->readFile(vertFilepath);
+    auto fragCode = this->readFile(fragFilepath);
+    createShaderModule(vertCode, &this->vertShaderModule);
+    createShaderModule(fragCode, &this->fragShaderModule);
+    VkPipelineShaderStageCreateInfo shaderStages[2];
+    shaderStages[0].sType =
+        VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    shaderStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
+    shaderStages[0].module = this->vertShaderModule;
+    shaderStages[0].pName = "main";
+    shaderStages[0].flags = 0;
+    shaderStages[0].pNext = nullptr;
+    shaderStages[0].pSpecializationInfo = nullptr;
+
+    shaderStages[1].sType =
+        VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    shaderStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    shaderStages[1].module = this->fragShaderModule;
+    shaderStages[1].pName = "main";
+    shaderStages[1].flags = 0;
+    shaderStages[1].pNext = nullptr;
+    shaderStages[1].pSpecializationInfo = nullptr;
+
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+    vertexInputInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertexInputInfo.vertexAttributeDescriptionCount = 0;
+    vertexInputInfo.vertexBindingDescriptionCount = 0;
+    vertexInputInfo.pVertexAttributeDescriptions = nullptr;
+    vertexInputInfo.pVertexBindingDescriptions = nullptr;
+
+    VkGraphicsPipelineCreateInfo pipelineInfo{};
+    pipelineInfo.sType =
+        VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.stageCount = 2;
+    pipelineInfo.pStages = shaderStages;
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &configInfo.inputAssemblyInfo;
+    pipelineInfo.pViewportState = &configInfo.viewportInfo;
+    pipelineInfo.pRasterizationState = &configInfo.rasterizationInfo;
+    pipelineInfo.pMultisampleState = &configInfo.multisampleInfo;
+    pipelineInfo.pColorBlendState = &configInfo.colorBlendInfo;
+    pipelineInfo.pDepthStencilState = &configInfo.depthStencilInfo;
+    pipelineInfo.pDynamicState = nullptr;
+
+    pipelineInfo.layout = configInfo.pipelineLayout;
+    pipelineInfo.renderPass = configInfo.renderPass;
+    pipelineInfo.subpass = configInfo.subpass;
+
+    pipelineInfo.basePipelineIndex = -1;
+    pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+    if (vkCreateGraphicsPipelines(
+            this->zwDevice.device(), VK_NULL_HANDLE, 1, &pipelineInfo,
+            nullptr, &this->graphicsPipeline) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create graphics pipeline");
     }
+}
+void ZWPipeline::createShaderModule(const std::vector<char>& code,
+                                    VkShaderModule* shaderModule) {
+    VkShaderModuleCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = code.size();
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+    if (vkCreateShaderModule(zwDevice.device(), &createInfo, nullptr,
+                             shaderModule) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create shader module");
+    }
+}
+
+PipelineConfigInfo ZWPipeline::defaultPipelineConfigInfo(
+    uint32_t width, uint32_t height) {
+    PipelineConfigInfo configInfo{};
+    configInfo.inputAssemblyInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    configInfo.inputAssemblyInfo.topology =
+        VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+    configInfo.viewport.x = 0.0f;
+    configInfo.viewport.y = 0.0f;
+    configInfo.viewport.width = static_cast<float>(width);
+    configInfo.viewport.height = static_cast<float>(height);
+    configInfo.viewport.minDepth = 0.0f;
+    configInfo.viewport.maxDepth = 1.0f;
+
+    configInfo.scissor.offset = {0, 0};
+    configInfo.scissor.extent = {width, height};
+
+    configInfo.viewportInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    configInfo.viewportInfo.viewportCount = 1;
+    configInfo.viewportInfo.pViewports = &configInfo.viewport;
+    configInfo.viewportInfo.scissorCount = 1;
+    configInfo.viewportInfo.pScissors = &configInfo.scissor;
+
+    configInfo.rasterizationInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    configInfo.rasterizationInfo.depthClampEnable =
+        VK_FALSE;  // clamp gl_Positon.z to  [0,1]
+    configInfo.rasterizationInfo.rasterizerDiscardEnable =
+        VK_FALSE;  // enable if only the earlier stage needed
+    configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+    configInfo.rasterizationInfo.lineWidth = 1.0f;
+    configInfo.rasterizationInfo.cullMode =
+        VK_CULL_MODE_NONE;  // discard tri based on winding order
+    configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
+    configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f;
+    configInfo.rasterizationInfo.depthBiasClamp = 0.0f;
+    configInfo.rasterizationInfo.depthBiasSlopeFactor = 0.0f;
+
+    configInfo.multisampleInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    configInfo.multisampleInfo.sampleShadingEnable = VK_FALSE;
+    configInfo.multisampleInfo.rasterizationSamples =
+        VK_SAMPLE_COUNT_1_BIT;
+    configInfo.multisampleInfo.minSampleShading = 1.0f;
+    configInfo.multisampleInfo.pSampleMask = nullptr;
+    configInfo.multisampleInfo.alphaToCoverageEnable = VK_FALSE;
+    configInfo.multisampleInfo.alphaToOneEnable = VK_FALSE;
+
+    configInfo.colorBlendAttachment.colorWriteMask =
+        VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+        VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    configInfo.colorBlendAttachment.blendEnable = VK_FALSE;
+    configInfo.colorBlendAttachment.srcColorBlendFactor =
+        VK_BLEND_FACTOR_ONE;  // Optional
+    configInfo.colorBlendAttachment.dstColorBlendFactor =
+        VK_BLEND_FACTOR_ZERO;  // Optional
+    configInfo.colorBlendAttachment.colorBlendOp =
+        VK_BLEND_OP_ADD;  // Optional
+    configInfo.colorBlendAttachment.srcAlphaBlendFactor =
+        VK_BLEND_FACTOR_ONE;  // Optional
+    configInfo.colorBlendAttachment.dstAlphaBlendFactor =
+        VK_BLEND_FACTOR_ZERO;  // Optional
+    configInfo.colorBlendAttachment.alphaBlendOp =
+        VK_BLEND_OP_ADD;  // Optional
+
+    configInfo.colorBlendInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    configInfo.colorBlendInfo.logicOpEnable = VK_FALSE;
+    configInfo.colorBlendInfo.logicOp = VK_LOGIC_OP_COPY;  // Optional
+    configInfo.colorBlendInfo.attachmentCount = 1;
+    configInfo.colorBlendInfo.pAttachments =
+        &configInfo.colorBlendAttachment;
+    configInfo.colorBlendInfo.blendConstants[0] = 0.0f;  // Optional
+    configInfo.colorBlendInfo.blendConstants[1] = 0.0f;  // Optional
+    configInfo.colorBlendInfo.blendConstants[2] = 0.0f;  // Optional
+    configInfo.colorBlendInfo.blendConstants[3] = 0.0f;  // Optional
+
+    return configInfo;
+}
 }  // namespace zw
